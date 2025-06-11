01f27be334bca1205b2957e09fa88aaedcb24425
elliotnis <elliotcylau@gmail.com>
2025-06-11 22:33:11 +0800

    added gitignore

---
+++ Assets/Scripts/Character/PlayerMovement.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 5f;
+    private CharacterController controller;
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        float moveX = Input.GetAxis("Horizontal"); // A/D or Left/Right Arrow
+        float moveZ = Input.GetAxis("Vertical");   // W/S or Up/Down Arrow
+
+        Vector3 move = transform.right * moveX + transform.forward * moveZ;
+        controller.Move(move * speed * Time.deltaTime);
+    }
+}
